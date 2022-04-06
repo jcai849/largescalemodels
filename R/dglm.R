ee99ca6872c3993329082a3f1001571e9753c312
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-04-07 10:49:14 +1200

    setup dglm

--- R/dglm.R
+++ R/dglm.R
@@ -1,40 +1,39 @@
 dglm <- function(formula, data, verbose=FALSE) {
-	stopifnot(is.distObjRef(data))
+	stopifnot(inherits(data, "DistributedObject"))
+
 	epsilon	<- 1e-08
 	maxit	<- 30
-	de	<- denvRef(data)
-	with(de, { # initialise
-		fam		<-stats::binomial()
-		beta_hat	<-NULL
-		mm		<-stats::model.matrix(form, data)
-		y		<-matrix(data[rownames(mm),all.vars(form)[1]],ncol=1)
-		NOBS		<-NROW(y)
-		WEIGHTS		<-rep.int(1,NROW(y))
-		OFFSET		<-rep.int(0,NROW(y))
-		mustart		<-(WEIGHTS*y+0.5)/(WEIGHTS+1)
-		eta		<-fam$linkfun(mustart)
-		mu		<-fam$linkinv(eta)
-		mu.eta.val	<-fam$mu.eta(eta)
-		z		<-(eta-OFFSET)+(y-mu)/mu.eta.val
-		w		<-sqrt((WEIGHTS*mu.eta.val^2)/fam$variance(mu))
-	  },
-	  list(formula=I(envBase(formula)), data=data), result=FALSE)
-	dev <- sum(with(de, fam$dev.resids(y, mu, WEIGHTS)))
-	for (iter in 1L:maxit) { # reweightin:
-		XtX <- emerge(with(de, crossprod(mm[,,drop=FALSE] * as.numeric(w))))
-		Xty <- emerge(with(de, t(mm[,,drop=FALSE] * as.numeric(w)) %*% (z * as.numeric(w))))
+
+	fam		<- stats::binomial()
+	beta_hat	<- NULL
+	mm		<- do.dcall(stats::model.matrix, list(formula, data))
+	y 		<- do.dcall(function(data, mm, formula) matrix(data[rownames(mm),all.vars(form)[1]],ncol=1), list(data=data, mm=mm, formula=formula))
+	NOBS 		<- NROW(y)
+	WEIGHTS		<- rep.int(1,NROW(y))
+	OFFSET		<- rep.int(0,NROW(y))
+	mustart		<- (WEIGHTS*y+0.5)/(WEIGHTS+1)
+	eta		<- do.dcall(fam$linkfun, list(mustart))
+	mu		<- do.dcall(fam$linkinv, list(eta))
+	mu.eta.val	<- do.dcall(fam$mu.eta, list(eta))
+	z		<-(eta-OFFSET)+(y-mu)/mu.eta.val
+	w		<-sqrt((WEIGHTS*mu.eta.val^2)/fam$variance(mu))
+	
+	dev <- sum(do.dcall(fam$dev.resids, list(y, mu, WEIGHTS)))
+
+	for (iter in 1L:maxit) { # reweighting:
+		XtX <- emerge(do.dcall(function(mm, w) crossprod(mm[,,drop=FALSE] * as.numeric(w)), list(mm, w)))
+		Xty <- emerge(do.dcall(function(mm, w, z) t(mm[,,drop=FALSE] * as.numeric(w)) %*% (z * as.numeric(w)), list(mm, w, z)))
 		beta_hat <- solve(XtX, Xty)
-		with(de, { # update beta hat
-			     eta	<- drop(mm %*% beta_hat) + OFFSET
-			     mu		<- fam$linkinv(eta)
-			     mu.eta.val	<- fam$mu.eta(eta)
-			     z		<- (eta - OFFSET) + (y - mu) / mu.eta.val
-			     w		<- sqrt((WEIGHTS * mu.eta.val^2) / fam$variance(mu))
-		  }, list(beta_hat=I(beta_hat)), result=FALSE)
+			eta	<- do.dcall(function(mm, beta_hat, OFFSET) drop(mm %*% beta_hat) + OFFSET, list(mm, beta_hat, OFFSET))
+			mu		<- do.dcall(fam$linkinv, list(eta))
+			mu.eta.val	<- do.dcall(fam$mu.eta, list(eta))
+			z		<-(eta-OFFSET)+(y-mu)/mu.eta.val
+			w		<-sqrt((WEIGHTS*mu.eta.val^2)/fam$variance(mu))
 		devold <- dev
-		dev <- sum(with(de, fam$dev.resids(y, mu, WEIGHTS)))
+		dev <- sum(do.dcall(fam$dev.resids, list(y, mu, WEIGHTS)))
 		if (verbose) cat("Deviance = ", dev, " Iterations - ", iter, "\n", sep = "")
 		if (abs(dev - devold)/(0.1 + abs(dev)) < epsilon) break
+		gc()
 	}
-	emerge(with(de, beta_hat))
+	emerge(beta_hat)
 }
