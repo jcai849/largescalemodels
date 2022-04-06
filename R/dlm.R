73d945d1874ff0b102fe8775ab64aa4382ee8a08
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-04-07 09:46:33 +1200

    prepare dlm for largerscale

--- R/dlm.R
+++ R/dlm.R
@@ -1,23 +1,39 @@
 dlm <- function(formula, data, weights=NULL, sandwich=FALSE) {
-	stopifnot(largeScaleR::is.distObjRef(data))
-	chunks <- largeScaleR::chunkRef(data)
-	stopifnot(length(chunks) > 0L)
-	dblm <- dbiglm(formula, chunks[[1]], weights, sandwich)
-	if (length(chunks) != 1L)
-		largeScaleR::dreduce(f="biglm::update.biglm",
-				     x=largeScaleR::distObjRef(chunks[-1]),
-				     init=dblm)
+	stopifnot(inherits(data, "DistributedObject"))
+	stopifnot(length(unclass(data)) > 0L)
+	init <- dbiglm(formula, data[[1]], weights, sandwich)
+	if (length(data) != 1L)
+		largescaler::dreduce(f=biglm::update.biglm,
+				     x=largescaler::data[-1],
+				     init=init)
 	else init
 }
 
 dbiglm <- function(formula, data, weights=NULL, sandwich=FALSE) {
-	stopifnot(largeScaleR::is.chunkRef(data))
-	sys.call <- largeScaleR::currCallFun(-1)
-	largeScaleR::do.ccall(what="biglm::biglm",
-			      args=list(formula=largeScaleR::envBase(formula),
-					data=data,
-					weights=largeScaleR::envBase(weights),
-					sandwich=sandwich),
-			      target=data,
-			      insert=list(sys.call=largeScaleR::envBase(sys.call)))
+	stopifnot(inherits(data, "DistributedObject"))
+	sys.call <- curr_call_fun(-1)
+	largescaler::do.dcall(largescalemodelr::biglm_fixed_call,
+			      list(formula=benv(formula),
+				   data=data,
+				   weights=benv(weights),
+				   sandwich=sandwich,
+				   sys.call=benv(sys.call)),
+			      target=data)
+}
+
+biglm_fixed_call <- function(formula, data, weights, sandwich, sys.call) {
+	rval <- biglm::biglm(formula, data, weights, sandwich)
+	rval$call <- sys.call
+	rval
+}
+
+curr_call_fun <- function(n=0) {
+	cl <- sys.call(n-1)
+	as.function(c(alist(...=), call("quote", cl)))
+}
+
+benv <- function(x) {
+        if (missing(x)) return(baseenv())
+        if (!is.null(x)) attr(x, ".Environment") <- baseenv()
+        x
 }
