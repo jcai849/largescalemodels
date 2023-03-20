29d8776d7df2e5d773288f93410197840dfe40b8
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-03-20 21:31:33 +1300

    shift write_load source

--- tests/dlasso.R
+++ tests/dlasso.R
@@ -1,6 +1,3 @@
-source("write_load.R")
-
-
 m <- 20
 n <- 50000
 N <- 5
@@ -10,6 +7,4 @@ x_actual <- matrix(0, m)
 x_actual[c(1, 3, 14, 15, 9),] <- c(27, 1, 8, 2, 82)
 b <- A %*% x_actual
 
-write_load_matrix(A)
-write_load_matrix(b)
-dtime <- system.time(dpielasso <- dlasso(A, b, tolerance=1, rho=3, lambda=rho))
+source("write_load.R")
