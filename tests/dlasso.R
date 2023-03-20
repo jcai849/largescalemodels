d834b21fef00ac33f0896978d88a0f69bc2251dd
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-03-20 21:31:52 +1300

    run dlasso test

--- tests/dlasso.R
+++ tests/dlasso.R
@@ -8,3 +8,7 @@ x_actual[c(1, 3, 14, 15, 9),] <- c(27, 1, 8, 2, 82)
 b <- A %*% x_actual
 
 source("write_load.R")
+dA <- write_load_matrix(A)
+db <- write_load_matrix(b)
+
+dtime <- system.time(dpielasso <- dlasso(dA, db, tolerance=1, rho=3, lambda=3))
