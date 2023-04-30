4e1f1de375d6a19be14a3f42a34156351c195ffa
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-04-30 20:01:41 +1200

    amend dlasso to use materialisation

--- R/dlasso.R
+++ R/dlasso.R
@@ -2,8 +2,6 @@ S <- function(kappa) function(alpha) pmax(alpha-kappa, 0) - pmax(-alpha-kappa, 0
 p_norm <- function(p) function(x) sum(abs(x)^p)^(1/p)
 l1_norm <- p_norm(1)
 
-
-d.x_u_init <- d(function(where, what) what)
 d.x_update <- d(function(x_prev, A, b, u_prev, rho, z_prev) {
                     p_norm <- function(p) function(x) sum(abs(x)^p)^(1/p)
                     l2_norm <- p_norm(2)
@@ -13,17 +11,15 @@ d.x_update <- d(function(x_prev, A, b, u_prev, rho, z_prev) {
                         function(x_prev) (1/2)*l2_norm(A %*% x_prev - b)^2 +
                                         (rho/2)*l2_norm(x_prev - z_prev + u_prev)^2)
                 })
-d.z_update <- d(function(u_prev, x_curr, z_curr) u_prev + x_curr - z_curr)
 
 dlasso <- function(A, b, tolerance=1, rho=3, lambda=rho) {
-    M_N <- dim(A)
-    m <- emerge(d(ncol)(A))[1]
+    M_N <- rev(dim(materialise(A)))
+    m <- ncol(A)
     S_z <- S(lambda/(rho*M_N[2]))
 
     z_prev <- x_prev <- u_prev <- rep(Inf, m)
     z_curr <- rep(1, m)
-    x_curr <- d.x_u_init(A, z_curr) # Huge issue when giving x_curr <- u_curr <- d.x_u_init... why?
-    u_curr <- d.x_u_init(A, z_curr)
+    x_curr <- distribute(z_curr, where=A); u_curr <- distribute(z_curr, where=A)
     i <- 0
     while (l1_norm(z_curr - z_prev) > tolerance) {
         cat("Iteration: ", i <- i+1, '\n')
@@ -31,9 +27,12 @@ dlasso <- function(A, b, tolerance=1, rho=3, lambda=rho) {
 
         x_curr <- d.x_update(x_prev, A, b, u_prev, rho, z_prev)
 
-        dim(x_curr) <- dim(u_curr) <- rev(M_N)
+        u_curr <- materialise(u_curr);   x_curr <- materialise(x_curr)
+        dim(x_curr) <- dim(u_curr) <- M_N
+        u_curr <- dematerialise(u_curr); x_curr <- dematerialise(x_curr)
+
         z_curr <- S_z(rowMeans(emerge(x_curr)) + rowMeans(emerge(u_curr)))
-        u_curr <- d.z_update(u_prev, x_curr, z_curr)
+        u_curr <- u_prev + x_curr - z_curr
     }
     z_curr
 }
