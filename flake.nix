30f3f034e33ec64be70c0947cc22af0b506cadf7
Jason Cairns <jason@cair.nz>
2025-06-23 01:29:16 +1200

    build: provide bundle and check in flake output

--- flake.nix
+++ flake.nix
@@ -35,15 +35,52 @@
           text = "exec radian";
         };
 
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-medium inconsolata;
+        };
+
+        bundle = pkgs.stdenv.mkDerivation {
+          name = "orcv-bundle";
+          nativeBuildInputs = [ R_dev ];
+          src = ./.;
+          buildPhase = ''
+            mkdir -p $out
+            R CMD build .
+            cp *.tar.gz $out/
+          '';
+          installPhase = "true";
+        };
+        check =
+          pkgs.runCommand "check"
+            {
+              nativeBuildInputs = [
+                R_dev
+                tex
+                pkgs.html-tidy
+              ];
+              buildInputs = [ bundle ];
+            }
+            ''
+              mkdir -p $out
+              R CMD check --as-cran ${bundle}/*.tar.gz
+              check_success=$?
+              cp -r *.Rcheck $out
+              exit $?
+            '';
+
       in
       {
-        packages.default = largescalemodels;
-        devShell = pkgs.mkShell {
+        packages = {
+          default = largescalemodels;
+          bundle = bundle;
+        };
+        devShells.default = pkgs.mkShell {
           buildInputs = [
             R_dev
             radian_dev_exec
           ];
         };
+        checks.default = check;
       }
     );
 }
